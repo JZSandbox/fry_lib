532b115b897d9f8ba96020f7948f9b132964c79a
JZSandbox <fryderyk.games@outlook.de>
2023-06-15 20:42:09 +0200

    Added on PlayerLoad, notification added

--- client/player/job.lua
+++ client/player/job.lua
@@ -1,7 +1,11 @@
 FRYLIB.JOB = {}
 local OBJECT = FRYLIB.INITEXPORT()
 local PED = nil
-local PLAYERDATA = OBJECT.Functions.GetPlayerData()
+local PLAYERDATA = nil
+
+AddEventHandler(Config.FRAMEWORK[Config.USEFRAMEWORK].PLAYER_LOAD, function()
+    PLAYERDATA = OBJECT.Functions.GetPlayerData()
+end)
 
 FRYLIB.JOB.GET = function()
     local DATA = PLAYERDATA.job
